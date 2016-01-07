ae41ce66039b043457deb6690d3596b22ea563d0
Phillip <Phillip@192.168.1.50>
2016-01-07 19:52:08 +1100

    add constructor, check for plugins folder, create if it doesn't exist

--- src/main/java/me/tekkitcommando/jc2/plugin/PluginLoader.java
+++ src/main/java/me/tekkitcommando/jc2/plugin/PluginLoader.java
@@ -1,13 +1,30 @@
 package me.tekkitcommando.jc2.plugin;
 
+import java.io.File;
+import me.tekkitcommando.jc2.JC2;
+
 /**
  * Created by ethan on 1/6/2016.
  */
 public class PluginLoader {
 
     /**
-     * Need to add the code for loading plugins
-     * made with the JC2 API and need to add code
-     * for requiring a plugin description file.
+     * Need to add the code for loading plugins made with the JC2 API and need
+     * to add code for requiring a plugin description file.
      */
+    File pluginDir = new File("plugins");  // Defining Directory/Folder Name  
+
+    public PluginLoader(JC2 parentApp) {
+        try {
+            if (!pluginDir.exists()) {  // Checks that Directory/Folder Doesn't Exists!  
+                boolean result = pluginDir.mkdir();
+                if (result) {
+                    System.out.println("New Folder created!");
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+
+        }
+    }
 }

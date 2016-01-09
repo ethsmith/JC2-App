10dcd24ca7e49943aa7576c332d9485255dafb4d
TekkitCommando <tekkitcommando@gmail.com>
2016-01-09 09:37:54 -0600

    Fuck dis shit, im out   凸

--- src/main/java/me/tekkitcommando/jc2/plugin/PluginLoader.java
+++ src/main/java/me/tekkitcommando/jc2/plugin/PluginLoader.java
@@ -1,9 +1,7 @@
 package me.tekkitcommando.jc2.plugin;
 
-import java.io.FileInputStream;
+import java.io.*;
 import java.lang.ClassLoader;
-import java.io.File;
-import java.io.FilenameFilter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.ZipEntry;
@@ -27,16 +25,22 @@ public class PluginLoader extends ClassLoader {
 
         File[] listOfFiles = pluginDir.listFiles(pluginFilter);
         for (File pluginFile : listOfFiles) {
-            // Don't worry about this for rn, I have a plan.
-        }
-
-        List<String> classNames = new ArrayList<String>();
-        ZipInputStream zip = new ZipInputStream(new FileInputStream("/path/to/jar/file.jar")); // Will be changed
-        for (ZipEntry entry = zip.getNextEntry(); entry != null; entry = zip.getNextEntry()) {
-            if (!entry.isDirectory() && entry.getName().endsWith(".class")) {
-                // This ZipEntry represents a class. Now, what class does it represent?
-                String className = entry.getName().replace('/', '.'); // including ".class"
-                classNames.add(className.substring(0, className.length() - ".class".length()));
+            List<String> classNames = new ArrayList<String>();
+            ZipInputStream zip = null;
+            try {
+                zip = new ZipInputStream(new FileInputStream("/" + pluginDir + "/" + pluginFile));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            try {
+                for(ZipEntry entry = zip.getNextEntry(); entry != null; entry = zip.getNextEntry()) {
+                    if(!entry.isDirectory() && entry.getName().endsWith(".class")) {
+                        String className = entry.getName().replace('/', '.');
+                        classNames.add(className.substring(0, className.length() - ".class".length()));
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }

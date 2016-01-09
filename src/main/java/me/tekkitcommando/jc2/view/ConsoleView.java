35f26c4362e210171336d61032bfb53d4ffaedee
Phillip <Phillip@192.168.1.50>
2016-01-09 12:47:54 +1100

    add ConsoleView

---
+++ src/main/java/me/tekkitcommando/jc2/view/ConsoleView.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.tekkitcommando.jc2.view;
+
+/**
+ *
+ * @author Phillip
+ */
+public interface ConsoleView {
+    
+    public void writeLine(String line);
+        
+    
+}
